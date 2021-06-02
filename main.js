32d05271deee89fe05070375fc90cee34a19d2b2
anup upreti <anupupreti32@gmail.com>
2021-06-02 15:31:37 +0545

    Enter

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ let ToDoContainer = document.getElementById('todocontainer');
 let inputField = document.getElementById('inputfield');
 
 addToDoButton.addEventListener('click', function(){
+    add();
+   });
+function Onenter(event,ele){
+    console.log(ele.value);
+    if (event.keyCode === 13) {
+        add();
+    }
+};
+function add(){
     var paragraph = document.createElement('p');
     paragraph.innerText = inputField.value;
     ToDoContainer.appendChild(paragraph);
-    inputField.value=" ";
+    inputField.value="";
+
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration="line-through";
     })
     paragraph.addEventListener('dblclick', function(){
         ToDoContainer.removeChild(paragraph);
     })
-})
+};
